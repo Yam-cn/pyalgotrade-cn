bae539ca53869b50131e7f3170351d21397694e0
chopchopjames <56582881@qq.com>
2016-03-06 22:37:01 +0800

    GO LIVE!!!

---
+++ stratlib/thrSMA_dayinfo.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 03 13:06:56 2015
+
+@author: Eunice
+"""
+
+if __name__ == '__main__':
+    import sys
+    sys.path.append("..")     
+    from pyalgotrade import bar
+    from pyalgotrade import plotter
+# 以上模块仅测试用
+from pyalgotrade.broker.fillstrategy import DefaultStrategy
+from pyalgotrade.broker.backtesting import TradePercentage
+from pyalgotrade import strategy
+from pyalgotrade.technical import ma
+from pyalgotrade.technical import cross
+
+class thrSMA(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, short_l, mid_l, long_l, up_cum):
+        strategy.BacktestingStrategy.__init__(self, feed)
+        self.__instrument = instrument
+        self.getBroker().setFillStrategy(DefaultStrategy(None))
+        self.getBroker().setCommission(TradePercentage(0.001))
+        self.__position = None
+        self.__prices = feed[instrument].getPriceDataSeries()
+        self.__malength1 = int(short_l)
+        self.__malength2 = int(mid_l)
+        self.__malength3 = int(long_l)
+        self.__circ = int(up_cum)
+        
+        self.__ma1 = ma.SMA(self.__prices, self.__malength1)
+        self.__ma2 = ma.SMA(self.__prices, self.__malength2)
+        self.__ma3 = ma.SMA(self.__prices, self.__malength3)
+        
+        self.__datetime = feed[instrument].getDateTimes()
+        self.__open = feed[instrument].getOpenDataSeries()
+        self.__high = feed[instrument].getHighDataSeries()
+        self.__low = feed[instrument].getLowDataSeries()
+        self.__close = feed[instrument].getCloseDataSeries()
+        
+    def getPrice(self):
+        return self.__prices
+
+    def getSMA(self):
+        return self.__ma1,self.__ma2, self.__ma3
+
+    def onEnterCanceled(self, position):
+        self.__position = None
+
+    def onEnterOK(self):
+        pass
+
+    def onExitOk(self, position):
+        self.__position = None
+        #self.info("long close")
+
+    def onExitCanceled(self, position):
+        self.__position.exitMarket()
+        
+    def buyCon1(self):
+        if cross.cross_above(self.__ma1, self.__ma2) > 0:
+            return True
+
+    def buyCon2(self):
+        m1 = 0
+        m2 = 0
+        for i in range(self.__circ):
+            if self.__ma1[-i-1] > self.__ma3[-i-1]:
+                m1 += 1
+            if self.__ma2[-i-1] > self.__ma3[-i-1]:
+                m2 += 1
+
+        if m1 >= self.__circ and m2 >= self.__circ:
+            return True
+    
+    def sellCon1(self):
+        if cross.cross_below(self.__ma1, self.__ma2) > 0:
+            return True
+            
+
+    def onBars(self, bars):
+        # If a position was not opened, check if we should enter a long position.
+        self.dayInfo(bars[self.__instrument])
+        
+        if self.__ma2[-1]is None:
+            return 
+            
+        if self.__position is not None:
+            if not self.__position.exitActive() and cross.cross_below(self.__ma1, self.__ma2) > 0:
+                self.__position.exitMarket()
+                #self.info("sell %s" % (bars.getDateTime()))
+        
+        if self.__position is None:
+            if self.buyCon1() and self.buyCon2():
+                shares = int(self.getBroker().getCash() * 0.2 / bars[self.__instrument].getPrice())
+                self.__position = self.enterLong(self.__instrument, shares)
+#                print bars[self.__instrument].getDateTime(), bars[self.__instrument].getPrice()
+                #self.info("buy %s" % (bars.getDateTime()))
+                
+    def dayInfo(self, bar):
+        try:
+            self.__openD[-1]
+        except AttributeError:
+            self.__openD = []
+            self.__highD = []
+            self.__lowD = []
+            self.__closeD = []
+            self.__upper_limit = []
+            self.__lower_limit = []
+            
+        if len(self.__datetime) < 2:
+            self.__openD.append(bar.getOpen())
+            self.__highD.append(self.__high[-1])
+            self.__lowD.append(self.__low[-1])
+            self.__closeD.append(self.__close[-1])                              
+            return
+            
+        # if another day
+        if self.__datetime[-1].date() != self.__datetime[-2].date():
+            self.__openD.append(bar.getOpen())
+            self.__highD.append(self.__high[-1])
+            self.__lowD.append(self.__low[-1])
+            self.__closeD.append(self.__close[-1]) 
+            self.__upper_limit.append(round(round(self.__closeD[-2] * 1.1 * 1000) / 10) / 100)
+            self.__lower_limit.append(round(round(self.__closeD[-2] * 0.9 * 1000) / 10) / 100) 
+            print self.__datetime[-1].date(), self.__datetime[-2].date(), self.__openD[-1]
+                             
+        elif self.__datetime[-1].date() == self.__datetime[-2].date():
+            if self.__high[-1] > self.__highD[-1]:
+                self.__highD[-1] = self.__high[-1]
+            
+            if self.__low[-1] < self.__lowD[-1]:
+                self.__lowD[-1] = self.__low[-1]
+            
+            self.__closeD[-1] = self.__close[-1]
+    
+    
+if __name__ == "__main__": 
+    strat = thrSMA    
+    instrument = '000001'
+    market = 'SZ'
+    fromDate = '20140101'
+    toDate ='20160101'
+    frequency = bar.Frequency.MINUTE
+    paras = [2, 20, 60, 10]
+    plot = True
+    
+    #############################################path set ############################33 
+    if frequency == bar.Frequency.MINUTE:
+        path = "..\\histdata\\min\\"
+    elif frequency == bar.Frequency.DAY:
+        path = "..\\histdata\\day\\"
+    filepath = path + instrument + market + ".csv"
+    
+    
+    #############################################don't change ############################33  
+    from pyalgotrade.barfeed.csvfeed import GenericBarFeed
+
+    
+    barfeed = GenericBarFeed(frequency)
+    barfeed.setDateTimeFormat('%Y-%m-%d %H:%M:%S')
+    barfeed.addBarsFromCSV(instrument, filepath)
+    strat = strat(barfeed, instrument, *paras)
+    
+    if plot:
+        plt = plotter.StrategyPlotter(strat, True, True, True)
+        
+    strat.run()
+    
+    if plot:
+        plt.plot()
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ stratlib/thrSMA_live.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 03 13:06:56 2015
+
+@author: Eunice
+"""
+
+if __name__ == '__main__':
+    import sys
+    sys.path.append("..")     
+    from pyalgotrade import bar
+    from pyalgotrade import plotter
+# 以上模块仅测试用
+from pyalgotrade.broker.fillstrategy import DefaultStrategy
+from pyalgotrade.broker.backtesting import TradePercentage
+from pyalgotrade import strategy
+from pyalgotrade.technical import ma
+from pyalgotrade.technical import cross
+
+class thrSMA(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, short_l, mid_l, long_l, up_cum):
+        strategy.BacktestingStrategy.__init__(self, feed)
+        self.__instrument = instrument
+        self.getBroker().setFillStrategy(DefaultStrategy(None))
+        self.getBroker().setCommission(TradePercentage(0.001))
+        self.__position = None
+        self.__prices = feed[instrument].getPriceDataSeries()
+        self.__malength1 = int(short_l)
+        self.__malength2 = int(mid_l)
+        self.__malength3 = int(long_l)
+        self.__circ = int(up_cum)
+        
+        self.__ma1 = ma.SMA(self.__prices, self.__malength1)
+        self.__ma2 = ma.SMA(self.__prices, self.__malength2)
+        self.__ma3 = ma.SMA(self.__prices, self.__malength3)
+        
+    def getPrice(self):
+        return self.__prices
+
+    def getSMA(self):
+        return self.__ma1,self.__ma2, self.__ma3
+
+    def onEnterCanceled(self, position):
+        self.__position = None
+
+    def onEnterOK(self):
+        pass
+
+    def onExitOk(self, position):
+        self.__position = None
+        #self.info("long close")
+
+    def onExitCanceled(self, position):
+        self.__position.exitMarket()
+        
+    def buyCon1(self):
+        if cross.cross_above(self.__ma1, self.__ma2) > 0:
+            return True
+
+    def buyCon2(self):
+        m1 = 0
+        m2 = 0
+        for i in range(self.__circ):
+            if self.__ma1[-i-1] > self.__ma3[-i-1]:
+                m1 += 1
+            if self.__ma2[-i-1] > self.__ma3[-i-1]:
+                m2 += 1
+
+        if m1 >= self.__circ and m2 >= self.__circ:
+            return True
+    
+    def sellCon1(self):
+        if cross.cross_below(self.__ma1, self.__ma2) > 0:
+            return True
+            
+
+    def onBars(self, bars):
+        # If a position was not opened, check if we should enter a long position.
+        
+        if self.__ma2[-1]is None:
+            return 
+            
+        if self.__position is not None:
+            if not self.__position.exitActive() and cross.cross_below(self.__ma1, self.__ma2) > 0:
+                self.__position.exitMarket()
+                #self.info("sell %s" % (bars.getDateTime()))
+        
+        if self.__position is None:
+            if self.buyCon1() and self.buyCon2():
+                shares = int(self.getBroker().getCash() * 0.2 / bars[self.__instrument].getPrice())
+                self.__position = self.enterLong(self.__instrument, shares)
+                print bars[self.__instrument].getDateTime(), bars[self.__instrument].getPrice()
+                #self.info("buy %s" % (bars.getDateTime()))
+
+def runStratOnTushare(strat, paras, security_id, market, frequency):    
+    from pyalgotrade.tushare.barfeed import TuShareLiveFeed
+    liveFeed = TuShareLiveFeed([security_id], frequency, 1024, 20)
+
+    strat = strat(liveFeed, security_id, *paras)
+        
+    strat.run()
+    
+if __name__ == "__main__": 
+    strat = thrSMA    
+    security_id = '000001'
+    market = 'SZ'
+    frequency = bar.Frequency.MINUTE
+    paras = [2, 20, 60, 10]
+    
+    runStratOnTushare(strat, paras, security_id, market, frequency)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
